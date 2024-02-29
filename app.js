53d218fa8df74eca7991d60681da3bcc1a4869a6
Hamza Ansari <hamzaans05@gmail.com>
2024-03-01 01:59:04 +0500

    created_if_else_conditions

---
+++ app.js
@@ -0,0 +1,21 @@
+// var dineramount = +prompt ("Bhai Mujhe Zinger Burger Khana Haie")
+// if (dineramount > 1000) {
+// console.log ("Bhai Maza a gaya Is may Tw Bhot Kch A jaiyee ga")
+// }
+
+// else if (dineramount >= 800 && dineramount <= 900) {
+// console.log("Yer is may tw only two hi zinger aye ge")
+// }
+
+// else{
+//     console.log(dineramount)
+// }
+
+var evenNum = +prompt("Inter Even Number");
+
+if(evenNum % 2 === 0) {
+    console.log("Number is Deivisible by 2")
+}
+else{
+    console.log("Number is Not Divisible 2")
+}
